e44f58efdb65098f6008eb81986408dde9d91005
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-06-26 16:04:33 -0300

    lembretes controller

--- src/Controllers/LembreteController.js
+++ src/Controllers/LembreteController.js
@@ -1,5 +1,45 @@
 const LembreteModel = require("../Models/LembreteModel");
 
-class LembreteController {}
+class LembreteController {
+    async getLembretesPorIdUsuario(req, res) {
+        const { id } = req.params;
+        try {
+            const lembretes = await LembreteModel.find({ id_usuario: id });
+
+            res.status(200).send(lembretes);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+    async createLembrete(req, res) {
+        const { id } = req.params;
+        const { titulo, descricao, data} = req.body;
+
+        try {
+            const tituloExiste = await LembreteModel.findOne({titulo, id_usuario: id});
+            if (tituloExiste) return res.status(403).send({message: "Título já existente!"});
+
+            await LembreteModel.create({id_usuario: id, ...req.body});
+
+            res.sendStatus(201);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+    async deleteLembrete(req, res) {
+        const { id } = req.params;
+        const { titulo } = req.body;
+        
+        try {
+            const lembreteExiste = await LembreteModel.findOne({titulo, id_usuario: id});
+            if (!lembreteExiste) return res.status(404).send("Lembrete não existe");
+
+            await LembreteModel.deleteOne({titulo, id_usuario: id})
+            res.sendStatus(202)
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+}
 
 module.exports = new LembreteController();
--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -3,21 +3,19 @@ const jwt = require("jsonwebtoken")
 
 class UsuarioController {
     async login(req, res) {
-        const {email} = req.body;
+        const { email } = req.body;
         try {
             const usuarioEncontrado = await UsuarioModel.findOne({ email });
             if(!usuarioEncontrado) return res.status(403).json({message: "Email invalido"});
 
             const usuario = usuarioEncontrado.toObject();
-            console.log(usuario)
             const token = jwt.sign({ usuario }, 
                 "68d3f0ee999d2717ea8e97a1f25e6055d467d8914c3766c9a7b74084970e05a0cafbb61f64a835497b0f385d20ac4418", 
                 { expiresIn: "1d" });
 
             res.status(200).json({ token });
         } catch (err) {
-            console.log(err);
-            res.status(500).send(err.data)
+            res.status(500).send(err)
         }
     }
 }
